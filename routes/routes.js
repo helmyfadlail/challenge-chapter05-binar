3a7451a27826e140836b0942b7857bbbbaa3edf9
helmy <helmyfadlail.5@gmail.com>
2023-05-04 20:59:29 +0700

    change name route

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,6 @@ router.use(require("./cars.js"));
 router.use(require("./superadmins.js"));
 router.use(require("./admins.js"));
 router.use(require("./members.js"));
-router.use(require("./auth.js"));
+router.use(require("./checkRole.js"));
 
 module.exports = router;
